a88b3d41d8e6424970ceaf5f2f961007d28d6a50
ASHMOR LAZARAGA <ashleymoriah.lazaraga@cit.edu>
2022-12-16 17:00:04 +0800

    Add LeapYear Components

--- src/LeapYear/LeapYearGUI.java
+++ src/LeapYear/LeapYearGUI.java
@@ -9,5 +9,16 @@ public class LeapYearGUI extends JFrame {
     private JTextField tfYear;
     private JButton btnCheckYear;
 
+
+    public static void main(String[] args) {
+        LeapYearGUI leap = new LeapYearGUI();
+
+        leap.setContentPane(leap.pnlMain);
+        leap.setSize(500, 500);
+        leap.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        leap.setVisible(true);
+        leap.setTitle("Leap Year Checker");
+
     }
+}
 
