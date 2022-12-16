d4a17f3b023b3eba42cbf5e8a61aa54578a1ef8a
ASHMOR LAZARAGA <ashleymoriah.lazaraga@cit.edu>
2022-12-16 18:16:00 +0800

    Complete FoodOrdering

--- src/FoodOrdering/FoodOrderGUI.java
+++ src/FoodOrdering/FoodOrderGUI.java
@@ -1,6 +1,10 @@
 package FoodOrdering;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class FoodOrderGUI  extends JFrame{
     private JPanel pnlMain;
@@ -16,6 +20,38 @@ public class FoodOrderGUI  extends JFrame{
     private JRadioButton rb10;
     private JRadioButton rb15;
 
+    List<JCheckBox> selecOrder;
+    List<JRadioButton> disCounts;
+
+
+    public FoodOrderGUI() {
+        selecOrder = new ArrayList<>();
+        selecOrder.add(cPizza);
+        selecOrder.add(cBurger);
+        selecOrder.add(cFries);
+        selecOrder.add(cSoftDrinks);
+        selecOrder.add(cTea);
+        selecOrder.add(cSundae);
+
+        disCounts = new ArrayList<>();
+        disCounts.add(rbNone);
+        disCounts.add(rb5);
+        disCounts.add(rb10);
+        disCounts.add(rb15);
+
+        btnOrder.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if(rbNone.isSelected()) {
+                    JOptionPane.showMessageDialog(pnlMain, String.format("The total price is Php %.2f", calcTotal()));
+                }
+                else {
+                    JOptionPane.showMessageDialog(pnlMain, String.format("The total price is Php %.2f", calcDiscount()));
+                }
+            }
+        });
+    }
+
 
     public static void main(String[] args) {
         FoodOrderGUI order = new FoodOrderGUI();
@@ -25,5 +61,54 @@ public class FoodOrderGUI  extends JFrame{
         order.setDefaultCloseOperation(EXIT_ON_CLOSE);
         order.setTitle("Food Ordering System");
         order.setVisible(true);
+    }
+
+    public double calcTotal() {
+        int prize = 0;
+        float total = 0;
+
+        if(cPizza.isSelected()) {
+            prize = 100;
+            total += prize;
+        }
+        if(cBurger.isSelected()) {
+            prize = 80;
+            total += prize;
+        }
+        if(cFries.isSelected()) {
+            prize = 65;
+            total += prize;
+        }
+        if(cSoftDrinks.isSelected()) {
+            prize = 55;
+            total += prize;
+        }
+        if(cTea.isSelected()) {
+            prize = 50;
+            total += prize;
+        }
+        if(cSundae.isSelected()) {
+            prize = 40;
+            total += prize;
+        }
+
+        return total;
+    }
+
+    public double calcDiscount() {
+        float initial = (float) calcTotal();
+        float finPrice = 0;
+
+        if(rb5.isSelected()) {
+            finPrice = (float)(initial * .95);
+        }
+        else if(rb10.isSelected()) {
+            finPrice = (float) (initial * .90);
+        }
+        else if(rb15.isSelected()) {
+            finPrice = (float) (initial * .85);
+        }
+
+        return finPrice;
     }
 }
